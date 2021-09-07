16e8e46cde63683db23195e021cfbb3563464616
GeorgePap <george.719pap@gmail.com>
2021-09-07 22:06:19 +0300

    Prototype for first_detailed_placement
    Signed-off-by: GeorgePap <george.719pap@gmail.com>

--- scripts/part2/DetailPlacement.py
+++ scripts/part2/DetailPlacement.py
@@ -1,12 +1,19 @@
-import operator
+from natsort import natsort_key
 
 from scripts.classes.Node import Node
 
 
 def first_detailed_placement(node_list: list, row_list: list, net_list: list):
-    print("TODO")
-
     for row in row_list:
-        # sort list alphabetical
-        sorted_list: list[Node] = sorted(row.row_nodes, key=operator.attrgetter('node_name'))
-        print(sorted_list)
+        # sort list alphabetical-numeric
+        # sorted_list: list[Node] = row.row_nodes
+        # sorted_list: list[Node] = natsorted(row.row_nodes, key=operator.attrgetter('node_name'))
+        # row.row_nodes.sort(key=natural_key())
+        # sorted_list: list[Node] = natsorted(row.row_nodes, key=lambda x: (not x.node_name.isdigit(), x.node_name))
+        sorted_list: list[Node] = sorted(row.row_nodes,
+                                         key=lambda x: (not x.node_name.isdigit(), natsort_key(x.node_name)))
+        # sorted_list: list[Node] = sorted(row.row_nodes, key=natural_key)
+        # natsort_key = natsort_keygen(key=lambda y: y.node_name)
+
+        for node in sorted_list:
+            print(node.node_name)
