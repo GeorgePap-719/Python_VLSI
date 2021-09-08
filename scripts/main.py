1bfeceaf53e50e2e2ce9ed49f9bf16e6e4a8994e
george papadopoulos <56389632+GeorgePap-719@users.noreply.github.com>
2021-09-08 21:08:57 +0300

    Experimental (#7)
    * Prototype for first_detailed_placement
    
    Signed-off-by: GeorgePap <george.719pap@gmail.com>

--- scripts/main.py
+++ scripts/main.py
@@ -2,9 +2,10 @@ import copy
 
 from scripts import functions
 from scripts.classes.Net import total_calculate_net_wirelength
-from scripts.part1.Legalization import lr_legalizing_tetris_like_algo, legalizing_tetris_like_algo
+from scripts.part1.Legalization import legalizing_tetris_like_algo
 from scripts.part1.Model import modeling
 from scripts.part1.Overlaps import count_overlaps_in_row_list
+from scripts.part2.DetailPlacement import first_detailed_placement
 
 if __name__ == "__main__":
     # change to appropriate path. If the path is not needed it can be left blank ("")
@@ -30,23 +31,32 @@ if __name__ == "__main__":
     legalized_node_list, legalized_row_list, legalized_net_list = legalizing_tetris_like_algo(node_list, row_list,
                                                                                               net_list)
 
-    modeling(legalized_node_list, legalized_row_list, legalized_net_list)
-
-    print("overlaps in legalized row_list")
-    print(count_overlaps_in_row_list(legalized_row_list))
+    print("first detailed placement")
+    detailed_placed_node_list, detailed_placed_row_list, detailed_placed_net_list = first_detailed_placement(
+        legalized_node_list, legalized_row_list, legalized_net_list)
 
     print("Wire length")
-    print(total_calculate_net_wirelength(legalized_net_list))
+    print(total_calculate_net_wirelength(detailed_placed_net_list))
+    #
+    # modeling(legalized_node_list, legalized_row_list, legalized_net_list)
+    #
+    # print("overlaps in legalized row_list")
+    # print(count_overlaps_in_row_list(legalized_row_list))
+    #
+    # print("Wire length")
+    # print(total_calculate_net_wirelength(legalized_net_list))
+    #
+    # print("Legalizing with second Tetris-like algorithm")
+    # legalized2_node_list, legalized2_row_list, legalized2_net_list = lr_legalizing_tetris_like_algo(lr_node_list,
+    #                                                                                                 lr_row_list,
+    #                                                                                                 lr_net_list)
+    #
+    # print("Second Wire length")
+    # print(total_calculate_net_wirelength(legalized2_net_list))
+    #
+    # modeling(legalized2_node_list, legalized2_row_list, legalized2_net_list)
+    #
+    # print("overlaps in second legalized row_list")
+    # print(count_overlaps_in_row_list(legalized2_row_list))
 
-    print("Legalizing with second Tetris-like algorithm")
-    legalized2_node_list, legalized2_row_list, legalized2_net_list = lr_legalizing_tetris_like_algo(lr_node_list,
-                                                                                                    lr_row_list,
-                                                                                                    lr_net_list)
-
-    print("Second Wire length")
-    print(total_calculate_net_wirelength(legalized2_net_list))
-
-    modeling(legalized2_node_list, legalized2_row_list, legalized2_net_list)
-
-    print("overlaps in second legalized row_list")
-    print(count_overlaps_in_row_list(legalized2_row_list))
+    first_detailed_placement(legalized_node_list, legalized_row_list, legalized_net_list)
--- scripts/part2/DetailPlacement.py
+++ scripts/part2/DetailPlacement.py
@@ -1,12 +1,43 @@
-import operator
+from natsort import *
 
+from scripts.classes.Net import total_calculate_net_wirelength
 from scripts.classes.Node import Node
+from scripts.part1.Legalization import update_net_list
 
 
 def first_detailed_placement(node_list: list, row_list: list, net_list: list):
-    print("TODO")
-
+    updated_net_list: list = net_list
     for row in row_list:
-        # sort list alphabetical
-        sorted_list: list[Node] = sorted(row.row_nodes, key=operator.attrgetter('node_name'))
-        print(sorted_list)
+        changes_flag = True
+
+        # we stop only when there are no more changes that lead to better result
+        while changes_flag:
+            changes_flag = False
+            """ Sort the given iterable in the way that humans expect."""
+            sorted_list: list[Node] = natsorted(row.row_nodes, key=lambda x: natsort_key(x.node_name))
+            for index, node in enumerate(sorted_list):
+                if index + 1 == len(sorted_list):
+                    break
+                for index2, node2 in enumerate(sorted_list):
+                    if index2 + 1 == len(sorted_list):
+                        continue
+                    if index2 == index:
+                        continue
+                    if node.node_width == node2.node_width:
+                        wirelength = total_calculate_net_wirelength(net_list)
+                        swap_positions(sorted_list, index, index2)
+                        updated_net_list = update_net_list(net_list, node_list)
+                        print("wirelength")
+                        new_wirelength = total_calculate_net_wirelength(updated_net_list)
+                        if new_wirelength > wirelength:
+                            changes_flag = True
+                            break  # make the first advantageous exchange
+                        else:
+                            # swap back positions
+                            swap_positions(sorted_list, index2, index)
+
+    return node_list, row_list, updated_net_list
+
+
+def swap_positions(data_list: list, position1: int, position2: int):
+    data_list[position1], data_list[position2] = data_list[position2], data_list[position1]
