8b1702740fa3b6d43b409588f2bd09d459e830a5
GeorgePap <george.719pap@gmail.com>
2021-09-10 21:15:27 +0300

    Update first_detailed_placement algo
    Signed-off-by: GeorgePap <george.719pap@gmail.com>

--- scripts/part2/DetailPlacement.py
+++ scripts/part2/DetailPlacement.py
@@ -1,3 +1,5 @@
+import copy
+
 from natsort import *
 
 from scripts.classes.Net import total_calculate_net_wirelength
@@ -6,35 +8,38 @@ from scripts.part1.Legalization import update_net_list
 
 
 def first_detailed_placement(node_list: list, row_list: list, net_list: list):
-    updated_net_list: list = net_list
-    for row in row_list:
-        changes_flag = True
+    updated_net_list: list = copy.deepcopy(net_list)
+    changes_flag = True
+    wirelength = total_calculate_net_wirelength(net_list)
 
-        # we stop only when there are no more changes that lead to better result
-        while changes_flag:
-            changes_flag = False
-            """ Sort the given iterable in the way that humans expect."""
-            sorted_list: list[Node] = natsorted(row.row_nodes, key=lambda x: natsort_key(x.node_name))
-            for index, node in enumerate(sorted_list):
-                if index + 1 == len(sorted_list):
-                    break
-                for index2, node2 in enumerate(sorted_list):
-                    if index2 + 1 == len(sorted_list):
-                        continue
-                    if index2 == index:
-                        continue
-                    if node.node_width == node2.node_width:
-                        wirelength = total_calculate_net_wirelength(net_list)
-                        swap_positions(sorted_list, index, index2)
-                        updated_net_list = update_net_list(net_list, node_list)
-                        print("wirelength")
-                        new_wirelength = total_calculate_net_wirelength(updated_net_list)
-                        if new_wirelength > wirelength:
-                            changes_flag = True
-                            break  # make the first advantageous exchange
-                        else:
-                            # swap back positions
-                            swap_positions(sorted_list, index2, index)
+    # we stop only when there are no more changes that lead to better result
+    while changes_flag:
+        changes_flag = False
+        """ Sort the given iterable in the way that humans expect."""
+        sorted_list: list[Node] = natsorted(node_list, key=lambda x: natsort_key(x.node_name))
+        for index, node in enumerate(sorted_list):
+            if index + 1 == len(sorted_list):
+                break
+            for index2, node2 in enumerate(sorted_list):
+                if index2 + 1 == len(sorted_list):
+                    continue
+                if index2 == index:
+                    continue
+                if node.node_width == node2.node_width:
+                    # swap_positions(sorted_list, index, index2)
+                    sorted_list[index], sorted_list[index2] = sorted_list[index2], sorted_list[index]
+                    updated_net_list = update_net_list(updated_net_list, node_list)
+                    print("wirelength")
+                    new_wirelength = total_calculate_net_wirelength(updated_net_list)
+                    if new_wirelength > wirelength:
+                        print("found better wirelength")
+                        wirelength = new_wirelength
+                        changes_flag = True
+                        break  # make the first advantageous exchange
+                    else:
+                        # swap back positions
+                        # swap_positions(sorted_list, index2, index)
+                        sorted_list[index2], sorted_list[index] = sorted_list[index], sorted_list[index2]
 
     return node_list, row_list, updated_net_list
 
